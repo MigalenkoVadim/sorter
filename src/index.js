e57926aa9f89649bde7094db94eec836ee368b5d
Vadim <vadik_mig@mail.ru>
2018-02-26 01:05:28 +0300

    make solution

--- src/index.js
+++ src/index.js
@@ -1,31 +1,44 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.arr = [];
+    this.comparator = function compareNumeric(a, b) {
+  if (a > b) return 1;
+  if (a < b) return -1;
+}
   }
 
   add(element) {
-    // your implementation
+    return this.arr.push(element);
   }
 
   at(index) {
-    // your implementation
+     return this.arr[index];
   }
 
   get length() {
-    // your implementation
+     return this.arr.length;
   }
 
   toArray() {
-    // your implementation
+    return this.arr;
   }
 
   sort(indices) {
-    // your implementation
+    var temp = [];
+    indices.sort();
+    for (var i = 0; i < indices.length; i++) {
+        temp.push(this.arr[indices[i]]);
+    }
+    temp.sort(this.comparator);
+    for(var i = 0; i < indices.length; i++) {
+        this.arr[indices[i]] = temp[0];
+        temp.shift();
+    }
   }
 
   setComparator(compareFunction) {
-    // your implementation
-  }
+      this.comparator = compareFunction;
+    }
 }
 
 module.exports = Sorter;
